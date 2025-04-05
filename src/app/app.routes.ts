ba8f1a33c1a2b227438cf473b91ccfdacd876b76
kkirans7272 <kkiranssai7272@gmail.com>
2025-04-05 15:00:59 -0400

    Annding fourth commit after changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // default page
+    { path: '', redirectTo: 'home', pathMatch: 'full'}, // default page
+    { path: 'home', component: HomeComponent }, // default page
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent }
   
