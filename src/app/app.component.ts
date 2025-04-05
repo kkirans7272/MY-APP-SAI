1bb395debc30abddd2110803e2685b8328ee81f1
kkirans7272 <kkiranssai7272@gmail.com>
2025-04-05 14:06:21 -0400

    Adding first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule ,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';  // Import Routes from Angular router
+import { HomeComponent } from './home/home.component';  // Import HomeComponent
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, // default page
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent }
+  
+];
